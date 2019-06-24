f3ca938de091589b53e5a5beb2454487e04ac924
BranTheBroken <50827666+BranTheBroken@users.noreply.github.com>
2019-06-24 10:09:56 +0530

    Add button and list

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("hey");
+document.getElementById("addItem").addEventListener("click",function(){
+	var value = document.getElementById("Item").value;
+	if(value)
+	{
+		console.log("there is a value");
+	}
+});
